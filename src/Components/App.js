97a2c1d11bb537920d7a49bc034de369080f3dd7
Aleksander Szostakowski <aleksander.szostakowski@gmail.com>
2023-02-28 21:32:04 +0100

    first commit

---
+++ src/Components/App.js
@@ -0,0 +1,15 @@
+import './App.css';
+import logotyp from '../Images/logotyp.png'
+
+function App() {
+  return (
+    <div className="App">
+       <img src={logotyp} ali="logo" className="logo"></img>
+       <p className="link-p">Strona w budowie. Zapraszam do owiedzenia starej wersji</p>
+       <p className="link-p">Website under construction. I invite you to visit the old verion</p>
+       <p className="link-p">Site en construction. Je vous invite à visiter l'ancienne version</p>
+    </div>
+  );
+}
+
+export default App;
