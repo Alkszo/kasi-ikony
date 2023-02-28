1515ffab73714c5e4e637ef29f14fbfc5428e623
Aleksander Szostakowski <aleksander.szostakowski@gmail.com>
2023-02-28 21:54:37 +0100

    empty links

--- src/Components/App.js
+++ src/Components/App.js
@@ -5,9 +5,9 @@ function App() {
   return (
     <div className="App">
        <img src={logotyp} ali="logo" className="logo"></img>
-       <p className="link-p">Strona w budowie. Zapraszam do owiedzenia starej wersji</p>
-       <p className="link-p">Website under construction. I invite you to visit the old verion</p>
-       <p className="link-p">Site en construction. Je vous invite à visiter l'ancienne version</p>
+       <p className="link-p">Strona w budowie. Zapraszam do owiedzenia <a href="/">starej wersji</a></p>
+       <p className="link-p">Website under construction. I invite you to visit the <a href="/">old verion</a></p>
+       <p className="link-p">Site en construction. Je vous invite à visiter <a href="/">l'ancienne version</a></p>
     </div>
   );
 }
